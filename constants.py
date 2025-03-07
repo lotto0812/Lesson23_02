f9aaf0e50eed93c24ce6f95ee28f0e6552b36a1f
Naoto Kato <my.dog.walk.on.air@gmail.com>
2025-03-07 14:04:54 +0900

    first commit

---
+++ constants.py
@@ -0,0 +1,23 @@
+APP_NAME = "問い合わせ対応自動化AIエージェント"
+STYLE = """
+<style>
+    .stHorizontalBlock {
+        margin-top: -14px;
+    }
+    .stChatMessage + .stHorizontalBlock {
+        margin-left: 56px;
+    }
+    .stChatMessage + .stHorizontalBlock .stColumn:nth-of-type(2) {
+        margin-left: -24px;
+    }
+    @media screen and (max-width: 480px) {
+        .stChatMessage + .stHorizontalBlock {
+            flex-wrap: nowrap;
+            margin-left: 56px;
+        }
+        .stChatMessage + .stHorizontalBlock .stColumn:nth-of-type(2) {
+            margin-left: -206px;
+        }
+    }
+</style>
+"""
